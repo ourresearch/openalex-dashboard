180f1eaa63eb6fb24670160136b769bd543b2911
h1-the-swan <jason.portenoy@gmail.com>
2024-03-27 08:01:32 -0400

    debug

--- sales/heroku_api.py
+++ sales/heroku_api.py
@@ -33,7 +33,7 @@ class HerokuAPI:
         r = requests.get(url, headers=headers)
         print(f"debug: status code: {r.status_code}")
         config_vars = r.json()
-        print(f"there are {len(config_vars["TOP_SECRET_UNLIMITED_EMAILS"])} emails")
+        print(f'there are {len(config_vars["TOP_SECRET_UNLIMITED_EMAILS"])} emails')
 
         r = requests.patch(url, headers=headers, json=update_dict)
         print(r)
